5e57d5aa940fb9498a4aa415ded6aef4d41f5ba0
igora <igora@DESKTOP-E1ODP0R>
2021-10-31 13:25:57 -0300

    Adicionar arquivos de projeto.

---
+++ Send/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "app",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+        int count = 0;
+        while (true)
+        {
+            string message = $"{count++} Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "app",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
